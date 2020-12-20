8a33bbb746a4e51e78cd7ebbd2be62a3d7c3ffe3
YashTotale <totaleyash@gmail.com>
2020-12-19 21:36:56 -0800

    Set up extension manifest & build script

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+build() {
+    echo 'Building Introspect...'
+
+    export INLINE_RUNTIME_CHUNK=false
+    export GENERATE_SOURCEMAP=false
+
+    react-scripts build
+
+    mv build/index.html build/popup.html
+}
+
+build

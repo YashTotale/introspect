b2c0d2c84fe5faf7203f040c4d325051ed8a2fe4
YashTotale <totaleyash@gmail.com>
2020-12-19 22:13:50 -0800

    Added Material UI & watch script

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+// React Imports
 import React, { FC } from "react";
+
+// Util Imports
+import { EXTENSION_NAME } from "./Utils/constants";
+
+// Material UI Imports
 import Theme from "./Theme";
-import {} from "@material-ui/core";
+import { makeStyles, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({}));
 
 const App: FC = () => {
-  return <Theme></Theme>;
+  const classes = useStyles();
+  return (
+    <Theme>
+      <Typography variant="h3">{EXTENSION_NAME}</Typography>
+    </Theme>
+  );
 };
 
 export default App;

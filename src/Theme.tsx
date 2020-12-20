af65cd28f34eb07d772ad0d048bc08e91a3748ee
YashTotale <totaleyash@gmail.com>
2020-12-19 22:08:56 -0800

    Added Material UI & watch script

---
+++ src/Theme.tsx
@@ -0,0 +1,36 @@
+//React Imports
+import React from "react";
+
+//Material UI Imports
+import {
+  useMediaQuery,
+  createMuiTheme,
+  ThemeProvider,
+  CssBaseline,
+} from "@material-ui/core";
+
+export const alternativeFont = "Arial, sans-serif";
+
+const Theme: React.FC = ({ children }) => {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = createMuiTheme({
+    typography: {
+      fontFamily: "Palatino, Georgia, Serif",
+      fontWeightBold: 600,
+    },
+    palette: {
+      type: prefersDarkMode ? "dark" : "light",
+    },
+    spacing: 8,
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default Theme;
